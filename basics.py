e74b22d56ae80c7e13fc8f322f75b998599bccd3
Otieno-G <genesisotieno827@gmail.com>
2026-06-06 20:25:57 +0300

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,12 @@
+#This is a comment, we use the #to indicate comments in python
+
+""" This is a multiline comment """
+
+#Python statements
+Print("Welcome to my github repository") #This is a statement
+
+#Many statement
+print("Welcome !")
+print("Lets dive into Python")
+print("what should we begin with?")
+
