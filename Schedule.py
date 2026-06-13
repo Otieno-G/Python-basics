56662c1b34846ab03ed85f437d469e5b7a38d096
Otieno-G <genesisotieno827@gmail.com>
2026-06-13 21:40:52 +0300

    Update Schedule.py

--- Schedule.py
+++ Schedule.py
@@ -1,11 +1,11 @@
 #To Do list
 
-rest = 1 hour
-database = 2hours
-python = 2 hours
-editing = 2 hours
-reading = 1 hour
-exercise = 20 mins
+rest_hours= 1 
+database_hours = 2
+python_hours = 2 
+editing_hours = 2 
+reading_hours = 1
+exercise_mins = 20
 
 total_hours = database + rest + python + editing + reading + exercise
 
