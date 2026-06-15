566cd3e69546ca7a02de646c9b0cd67a80152e0a
Otieno-G <genesisotieno827@gmail.com>
2026-06-15 12:07:10 +0300

    Update Schedule.py

--- Schedule.py
+++ Schedule.py
@@ -5,7 +5,7 @@ database_hours = 2
 python_hours = 2 
 editing_hours = 2 
 reading_hours = 1
-exercise_mins = 20
+exercise_mins = 20/60
 
 total_hours = database + rest + python + editing + reading + exercise
 
